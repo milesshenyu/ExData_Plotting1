bea85c1e284aebcd9349a8ad9f7b2f30732f0a60
milesshenyu <mileszhangpro@outlook.com>
2024-07-12 17:42:02 -0600

    add the comma

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 #Reading and subsetting
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
                    dec = ".", na.strings = "?")
-data <- data[data$Date %in% c("1/2/2007","2/2/2007")]
+data <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
 
 #Plotting and saving as PNG file
 png(filename = "plot3.png", width = 480, height = 480)
